8c814dd9eb01780830286862df4963984dbd9703
abondoa@gmail.com <abondoa@gmail.com@88672a42-5410-7d64-d5bb-e94785e5015c>
2012-10-19 08:18:23 +0000

    merged

--- src/emagent/agent/brp/IBrpBiddingStrategy.java
+++ src/emagent/agent/brp/IBrpBiddingStrategy.java
@@ -4,6 +4,6 @@ import emagent.auction.AuctionList;
 
 public interface IBrpBiddingStrategy {
 	
-	public int bidOnAuctions(AuctionList auctions, int monetaryBalance,
+	public void bidOnAuctions(AuctionList auctions, int monetaryBalance,
 			int electricalBalance, IBrp bidder) throws Exception;
 }
